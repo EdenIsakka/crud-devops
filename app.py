b2f56c0b4203dc9eaac9e4f73eb0f57174c8b993
EdenIsakka <julianig84@hotmail.com>
2025-02-15 17:39:07 -0500

    Recepcion de valores entrada

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 from flaskext.mysql import MySQL
 
 
@@ -23,6 +23,28 @@ def index():
     conn.commit()
 
     return render_template('medicos/index.html') 
+
+@app.route('/create')
+def create():
+
+    return render_template('medicos/create.html')
+
+@app.route('/store', methods=['POST'])
+def storage():
+    _nombre = request.form['txtNombre']
+    _correo= request.form['txtCorreo']
+
+    _foto = request.files['txtFoto']
+
+    
+    sql = "INSERT INTO `medicos` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL,%s,%s,%s);"
+
+    datos=(_nombre,_correo,_foto.filename)
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql,datos)
+    conn.commit()
 
 if __name__ == '__main__':
     app.run(debug=True)
